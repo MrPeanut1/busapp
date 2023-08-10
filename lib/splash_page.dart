790e44d23724b85bc36d8c94156832c4031e8e96
Moegamat Tashreeq Daniels 222818506 <85939836+MrPeanut1@users.noreply.github.com>
2023-08-10 15:07:31 +0200

    Login authentication

---
+++ lib/splash_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome', // Customize this text
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the login page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: const Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:busapp/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+<<<<<<< Updated upstream
 import 'package:busapp/main.dart';
 
+=======
+>>>>>>> Stashed changes
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
